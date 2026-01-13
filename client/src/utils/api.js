7b8dece56e4a4588d274e60b264960132f017259
Anmol chauhan <anmolchauhan0810@gmail.com>
2026-01-13 23:03:39 +0530

    Update TravelMate frontend and backend

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000";
+const API_URL = "https://travelmate-iwo2.onrender.com";
 
 /* ================= AUTH ================= */
 export const loginUser = async (email, password) => {
